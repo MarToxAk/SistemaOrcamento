b55b3eafbd1d7b7865e755450190bc86693881f1
José J <junior.ilha@hotmail.com.br>
2026-06-15 17:29:40 -0300

    feat(33-01): criar AthosProdutoService com criarProduto/editarProduto/alterarStatusProduto
    - AthosProdutoService com pool lazy-init proprio (padrao AthosService L500-528)
    - criarProduto: INSERT dinamico com RETURNING idproduto (sem LOCK TABLE - serial)
    - Pre-validacao FK para iddepartamento/idgrupo/idmarca via SELECT 1
    - Catch 23503 distingue FK de usuario (500 config) de FK de input (422 com constraint)
    - editarProduto: UPDATE parcial com idusuarioalteracao sempre incluido
    - alterarStatusProduto: toggle statusproduto/vendeproduto sem DELETE fisico
    - AthosProdutoService registrado em providers e exports de AthosModule

--- apps/backend/src/modules/integrations/athos/athos.module.ts
+++ apps/backend/src/modules/integrations/athos/athos.module.ts
@@ -6,12 +6,13 @@ import { QuotesModule } from "../../quotes/quotes.module";
 import { AthosController } from "./athos.controller";
 import { AthosListenerService } from "./athos-listener.service";
 import { AthosService } from "./athos.service";
+import { AthosProdutoService } from "./athos-produto.service";
 import { ProdutoController } from "./athos-produto.controller";
 
 @Module({
   imports: [DatabaseModule, EventsModule, forwardRef(() => QuotesModule)],
-  providers: [AthosService, AthosListenerService],
+  providers: [AthosService, AthosListenerService, AthosProdutoService],
   controllers: [AthosController, ProdutoController],
-  exports: [AthosService],
+  exports: [AthosService, AthosProdutoService],
 })
 export class AthosModule {}
